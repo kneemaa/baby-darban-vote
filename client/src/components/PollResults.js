c8ff40095974bdb92134f6ba9c3e002213240e8e
Nema Darban <nema.darban@gmail.com>
2021-11-15 20:27:51 -0800

    pollResults can now get data, ready for graph and  styling (#2)

--- client/src/components/PollResults.js
+++ client/src/components/PollResults.js
@@ -1,46 +1,12 @@
-import React, { Component } from "react"
-// import API from "../utils/api"
+import React from "react"
 
-class PollResults extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      boys: 0,
-      girls: 0
-    };
-  }
-
-  componentDidMount() {
-    this.breakDownGender()
-  }
-
-  breakDownGender() {
-    const genders = this.state.genders
-    console.log(genders)
-    for (let entry in genders) {
-      if (genders[entry].gender === 'girl') {
-        this.setState((state) => {
-          return { girls: state.girls + 1}
-        })
-      }
-
-      if (genders[entry].gender === 'boy') {
-        this.setState((state) => {
-          return { boys: state.boys + 1}
-        })
-      }
-    }
-  }
-
-  render() {
-    return (
+export const PollResults = (props) => {
+  return (
       <div>
-        <p>{this.state.girls}</p>
+        <h2> Poll Results! </h2>
+        <p>Boys: {props.boys}</p>
+        <p>Girls: {props.girls}</p>
         {/* maybe something like this? https://canvasjs.com/react-charts/bar-chart/ */}
       </div>
     )
-  }
 }
-
-export default PollResults
-
