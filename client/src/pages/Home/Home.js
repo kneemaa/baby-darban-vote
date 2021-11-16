ee2d66fe9c89ab80575162d87ebbd54b01f9b27a
Nema Darban <nema.darban@gmail.com>
2021-11-16 15:08:15 -0800

    conditional in place for displaying, cookies also added :)

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react"
 import API from "../../utils/api"
-import { PollResults } from "../../components/PollResults"
 import Chart from "../../components/Chart"
-import { CountDownTimer } from "../../components/CountDownTimer"
-import { FormBtn, Input } from "../../components/Form"
+import { CircleTimer } from "../../components/CircleTimer"
+import PollQuestion from "../../components/PollQuestion"
+import {Announce} from "../../components/Announce"
+// import { FormBtn, Input } from "../../components/Form"
+import moment from "moment"
+import Cookies from 'universal-cookie'
 import "../../App.css"
 
 class Home extends Component {
@@ -13,7 +16,9 @@ class Home extends Component {
     boys: 0,
     timeLeft: 1000,
     gender: "",
-    author: ""
+    author: "",
+    ballotCast: false,
+    endTime: "2021-11-16 16:00:00"
   }
 
   componentDidMount() {
@@ -26,6 +31,11 @@ class Home extends Component {
       .then(res => this.setState({genders: res.data, girls: 0, boys: 0, gender: "", author: ""}))
       .catch(err => console.log(err))
       .then(res => this.breakDownGender())
+
+      const cookies = new Cookies()
+      if (cookies.get('ballotCast')) {
+        this.setState({ballotCast: true})
+      }
   }
 
   breakDownGender() {
@@ -46,64 +56,41 @@ class Home extends Component {
   }
 
   getTimeLeft() {
-    const countDownDate = new Date("Nov 18, 2021 22:00:00").getTime()
-    let now = new Date().getTime();
-    let distance = countDownDate - now;
+    const announceTime = moment(this.state.endTime)
+    const distance = announceTime.diff(moment(), "seconds")
     this.setState({timeLeft: distance})
   }
 
-
-  setGender(event) {
-    // console.log(event.target.value)
-    this.setState({guess:event.target.value})
+  handleTimeUpdate = (childData) => {
+    this.setState({timeLeft: childData})
   }
 
-  castVote = event => {
-    event.preventDefault()
-    if (this.state.author && this.state.gender) {
-      API.vote({
-        gender: this.state.gender,
-        author: this.state.author
-      })
-      .then(res => console.log('vote cast for ' + this.state.author))
-      .then(res => this.loadResults())
-      .catch(err => console.log(err))
-    }
-  }
-
-  postCastedVote = event => {
-    const { name, value } = event.target
-    this.setState({
-      [name]: value
-    })
+  handleCastedVote = (childData) => {
+    const cookies = new Cookies()
+    this.setState(childData)
+    this.loadResults()
+    cookies.set('ballotCast', {path: '/'})
   }
 
   render() {
     return (
       <div>
-        <CountDownTimer timeLeft={this.state.timeLeft}/>
-        <div className="PollQuestion">
-          <p color="blue"> I'm the poll QUESTION section</p>
-          <form>
-            <Input 
-              value={this.state.author}
-              onChange={this.postCastedVote}
-              placeholder="what's your name?"
-              name="author"
-            />
-            <FormBtn
-              onClick={this.castVote}
-            >
-              Cast Vote
-            </FormBtn>
-            <div onChange={this.postCastedVote.bind(this)}>
-              <input type="radio" value="boy" name="gender"/> Boy
-              <input type="radio" value="girl" name="gender"/> Girl
-            </div>
-          </form>
-        </div>
-        <Chart boys={this.state.boys} girls={this.state.girls}/>
-        <PollResults boys={this.state.boys} girls={this.state.girls}/>
+        {this.state.timeLeft <= 0 ? (
+          <div>
+            {this.state.ballotCast ? (
+              <div>
+                <Chart boys={this.state.boys} girls={this.state.girls}/>
+                <Announce/>
+              </div>
+            ) : (<PollQuestion voteCallback={this.handleCastedVote}/>)}
+          </div>
+        ) : (
+          <div>
+          <CircleTimer endTime={this.state.endTime}/>
+            {!this.state.ballotCast ? (<PollQuestion voteCallback={this.handleCastedVote}/>) : (<Chart boys={this.state.boys} girls={this.state.girls}/>)}
+          </div>
+          )
+        }
       </div>
     )
   }
