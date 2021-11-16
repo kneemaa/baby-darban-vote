1859c6940e183a48b54a324defea824af5766834
Nema Darban <nema.darban@gmail.com>
2021-11-15 22:09:32 -0800

    countdown clock is functional and can be restyled

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react"
 import API from "../../utils/api"
-import {PollResults} from "../../components/PollResults"
+import { PollResults } from "../../components/PollResults"
 import PollQuestion from "../../components/PollQuestion"
+import { CountDownTimer } from "../../components/CountDownTimer"
+import "../../App.css"
 
 class Home extends Component {
   state = {
     genders: [],
     girls: 0,
-    boys: 0
+    boys: 0,
+    timeLeft: 1000
   }
 
   componentDidMount() {
     this.loadResults()
+    this.getTimeLeft()
   };
 
   loadResults = () => {
@@ -38,9 +42,18 @@ class Home extends Component {
     }
   }
 
+  getTimeLeft() {
+    const countDownDate = new Date("Jan 5, 2022 15:37:25").getTime()
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+    console.log(distance)
+    this.setState({timeLeft: distance})
+  }
+
   render() {
     return (
       <div>
+        <CountDownTimer timeLeft={this.state.timeLeft}/>
         <PollQuestion/>
         <PollResults boys={this.state.boys} girls={this.state.girls}/>
       </div>
