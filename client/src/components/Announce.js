fec38acaece2f0b0001dd708b3b70e49999468c5
Nema Darban <nema.darban@gmail.com>
2021-11-16 23:27:18 -0800

    add Google Calendar event

--- client/src/components/Announce.js
+++ client/src/components/Announce.js
@@ -2,6 +2,12 @@ import React from "react"
 
 export const Announce = () => {
   return (
-    <p>we're having baby pizza!</p>
+    <div>
+      <p>we're having baby pizza!</p>
+      <a target="_blank" href="https://calendar.google.com/event?action=TEMPLATE&amp;tmeid=M2NmajN2bnNtZ2o4b2twMnZ0dnZudW1tZjQgbmVtYUBkYXJiYW4uZGV2&amp;tmsrc=nema%40darban.dev">
+        <img border="0" src="https://www.google.com/calendar/images/ext/gc_button1_en.gif"/>
+        Save the Date! Add to Google Calendar
+      </a>
+    </div>
   )
 }
