9664024717af04e2c863ca9d2199e0839a079839
Nema Darban <nema.darban@gmail.com>
2021-11-17 00:16:49 -0800

    trying to fix routes in heroku

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,9 @@ const controller = require('../controllers/genderControllers')
 const routes = (app) => {
   app.post('/api/vote', controller.vote)
   app.get('/api/genders', controller.findAll)
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));    
+  })
 }
 
 module.exports = routes
