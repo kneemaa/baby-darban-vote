ccbc0ae65a75ea4c193329ada6c5fff7809e4ddc
Frankey <frankeydarban@gmail.com>
2021-11-17 22:25:32 -0800

    centered vote button

--- client/src/components/Form/FormBtn.js
+++ client/src/components/Form/FormBtn.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 export const FormBtn = props =>
-  <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn btn-success">
+  <button {...props} className="btn btn-success">
     {props.children}
   </button>;
