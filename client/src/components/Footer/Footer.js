f2a8d3888e517032ef73b2c81d934e71c7a3f386
Nema Darban <nema.darban@gmail.com>
2021-11-18 12:26:24 -0800

    correct class to className

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -7,8 +7,8 @@ export const Footer = () => {
       <h4>Arriving April 24, 2022!</h4>
         <a target="_blank" rel="noreferrer" href="https://calendar.google.com/event?action=TEMPLATE&tmeid=NWVwMGZsaWhwaTJhbnRyYWM1NWdvYWYwcjcgY19sMThldWZndTBudGptbDM2ZGgzZTIxbDJia0Bn&tmsrc=c_l18eufgu0ntjml36dh3e21l2bk%40group.calendar.google.com">
           {/* <img border="0" src="https://www.google.com/calendar/images/ext/gc_button1_en.gif"/> */}
-        <div class="d-grid gap-2">
-          <button class="btn btn-lg btn-info" type="button">Click to add to your Google Calendar</button>
+        <div className="d-grid gap-2">
+          <button className="btn btn-lg btn-info" type="button">Click to add to your Google Calendar</button>
         </div>
         </a>
     </div>
