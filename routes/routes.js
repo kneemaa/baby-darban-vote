a36fe2c72d0a0a680dd12b4606d8836bda7336ee
Nema Darban <nema.darban@gmail.com>
2021-11-18 08:39:05 -0800

    reveal api route in server

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ const controller = require('../controllers/genderControllers')
 const routes = (app) => {
   app.post('/api/vote', controller.vote)
   app.get('/api/genders', controller.findAll)
+  app.get('/api/reveal', controller.getFinalAnswer)
 }
 
 module.exports = routes
