85d971f92056da732b3eb6c880213da7ba81cc80
Nema Darban <nema.darban@gmail.com>
2021-11-18 14:23:14 -0800

    winners table

--- controllers/genderControllers.js
+++ controllers/genderControllers.js
@@ -4,19 +4,26 @@ module.exports = {
   findAll: function(req, res) {
     db.Gender
       .find({})
-      .sort({ date: -1})
+      .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err))
   },
   vote: function(req, res) {
     db.Gender
-      .create(req.body)
+      .create( req.body )
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err))
+  },
+  findWinners: function(req, res) {
+    db.Gender
+      .find({ gender: req.params.reveal} )
+      .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err))
   },
   getFinalAnswer: function(req, res) {
     db.Reveal
-      .findOne({answer:"secret-answer"})
+      .findOne({ answer: "secret-answer" })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err))
   }
